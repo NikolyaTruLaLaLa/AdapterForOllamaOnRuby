54df0ccfdc36474efd2b6a842a78aedf982a6ebb
NikolayIgnat <1126ignatenkonikolay@gmail.com>
2025-06-03 15:30:40 +0300

    changed yamal

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,10 +1,10 @@
 require 'minitest/autorun'
 
-require 'webmock/minitest'  # Это ключевое изменение!
-require_relative '../Adapter'  # Или правильный путь к вашему файлу
+require 'webmock/minitest'  
+require_relative '../Adapter' 
 
 # Включаем WebMock и разрешаем только локальные соединения
 WebMock.enable!
 WebMock.disable_net_connect!(allow_localhost: true)
-# Добавьте пути к вашему коду
+
 $LOAD_PATH.unshift File.expand_path('../Adapter.rb')
